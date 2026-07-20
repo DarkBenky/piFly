950c82d895094e842ca356f6a9601a764d7b441a
darkBenky <matus.benky@gmail.com>
2026-07-20 23:21:23 +0200

    Add temperature module with BME280 sensor support

---
+++ modules/temperature.py
@@ -0,0 +1,17 @@
+import time
+import board
+import digitalio
+from adafruit_bme280 import basic as adafruit_bme280
+
+
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D5)
+bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
+
+if __name__ == "__main__":
+    while True:
+        print("Temperature: %0.1f C" % bme280.temperature)
+        print("Pressure: %0.1f hPa" % bme280.pressure)
+        print("Humidity: %0.1f %%" % bme280.relative_humidity)
+        print("")
+        time.sleep(2)
