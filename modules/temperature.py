7a8e407a15924dbe3e86f50622970a31d260154c
darkBenky <matus.benky@gmail.com>
2026-07-21 00:54:02 +0200

    Add temperature data logging to file

--- modules/temperature.py
+++ modules/temperature.py
@@ -8,10 +8,13 @@ spi = board.SPI()
 cs = digitalio.DigitalInOut(board.D5)
 bme280 = adafruit_bme280.Adafruit_BME280_SPI(spi, cs)
 
+
 if __name__ == "__main__":
     while True:
         print("Temperature: %0.1f C" % bme280.temperature)
         print("Pressure: %0.1f hPa" % bme280.pressure)
         print("Humidity: %0.1f %%" % bme280.relative_humidity)
         print("")
-        time.sleep(2)
+        with open("../logs/temperature.log", "a") as f:
+            f.write(f"{time.time()},{bme280.temperature},{bme280.pressure},{bme280.relative_humidity}\n")
+        time.sleep(30)
